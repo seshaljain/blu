15599782bbac48af378627d9ffe75ffbb0151a79
Seshal Jain <seshaljain11@gmail.com>
2022-07-17 17:37:38 +0530

    Update layouts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,9 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   content: ["./content/**/*.{html,js,md}", "./layouts/**/*.{html,js}"],
   theme: {
-    extend: {},
   },
   plugins: [require("@tailwindcss/typography")],
 };
